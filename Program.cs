ef4aa545cc4130476c79ce9a4d3a0ccc1588d539
Lyz <luisnunes371@gmail.com>
2024-03-09 23:23:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Hello, world!";
+        string reversed = ReverseString(input);
+        Console.WriteLine(reversed);
+    }
+
+    static string ReverseString(string input)
+    {
+        char[] chars = input.ToCharArray();
+        int left = 0;
+        int right = chars.Length - 1;
+
+        while (left < right)
+        {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+
+        return new string(chars);
+    }
+}
